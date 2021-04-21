32b8252191766fe6b94447b9a3374413b4615d8b
Heikki Miinalainen <heikki@swaymusic.co>
2021-04-21 10:54:21 +0300

    Added fancy OLED things

--- keyboards/kyria/keymaps/Hegezcc/config.h
+++ keyboards/kyria/keymaps/Hegezcc/config.h
@@ -18,13 +18,15 @@
 
 #ifdef OLED_DRIVER_ENABLE
   #define OLED_DISPLAY_128X64
+  #define OLED_FONT_H "keyboards/kyria/keymaps/Hegezcc/glcdfont.c"
 #endif
 
 #ifdef RGBLIGHT_ENABLE
-  #define RGBLIGHT_ANIMATIONS
-  #define RGBLIGHT_HUE_STEP 8
-  #define RGBLIGHT_SAT_STEP 8
-  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+  #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+  #define RGBLIGHT_HUE_STEP 16
+  #define RGBLIGHT_SAT_STEP 16
+  #define RGBLIGHT_VAL_STEP 16
   #define RGBLIGHT_LIMIT_VAL 150
 #endif
 
@@ -34,12 +36,46 @@
 // Trackball
 #define TRACKBALL_MATRIX_ROW 6
 #define TRACKBALL_MATRIX_COL 1
-#define TRACKBALL_RGBLIGHT 10
 #define TRACKBALL_ORIENTATION 2
+#ifdef RGBLIGHT_ENABLE
+	#define TRACKBALL_RGBLIGHT 11
+#endif
 
 // Rotary encoder resolution fix
 #define ENCODER_RESOLUTION 2
 
-// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-// #define SPLIT_USB_DETECT
-// #define NO_USB_STARTUP_CHECK
+// Custom tap-hold handler
+#define TAPHOLD_TIME 200
+
+// OLED info script
+#define INFO_OLED_ENABLE
+#define INFO_LINE_COUNT 6   // Should correspond to OLED line count allowed to program (max height - 2)
+#define INFO_LINE_LENGTH 21 // Should correspond to OLED char width
+#define INFO_BUFFER_SIZE (INFO_LINE_COUNT * INFO_LINE_LENGTH)
+
+#ifdef WPM_ENABLE
+  #define BONGOCAT_ENABLE
+#endif
+
+#ifdef BONGOCAT_ENABLE
+// From https://github.com/Dakes/kyria/blob/main/keymaps/dakes/keymap.c
+// originally by j-inc: https://github.com/qmk/qmk_firmware/blob/master/keyboards/kyria/keymaps/j-inc/keymap.c
+// Adapted to modular system and diffs using uint16_t arrays by Dakes (Daniel Ostertag).
+
+  // WPM-responsive animation stuff here
+  #define IDLE_FRAMES 5 // 5
+  #define IDLE_SPEED 20 // below this wpm value your animation will idle
+
+  #define PREP_FRAMES 1 // uncomment if >1
+
+  #define TAP_FRAMES 2
+  #define TAP_SPEED 40 // above this wpm value typing animation to triggered
+
+  #define PREP_FRAMES 1
+
+  #define ANIM_FRAME_DURATION 200 // how long each frame lasts in ms 200
+  // #define SLEEP_TIMER 60000 // should sleep after this period of 0 wpm, needs fixing
+
+  // OLED width
+  #define WIDTH 128
+#endif // BONGOCAT_ENABLE
--- keyboards/kyria/keymaps/Hegezcc/rules.mk
+++ keyboards/kyria/keymaps/Hegezcc/rules.mk
@@ -1,6 +1,22 @@
-OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-LTO_ENABLE = yes           # Allow link time optimization
-TRACKBALL_ENABLE = yes     # Trackball
+OLED_DRIVER_ENABLE = yes    # Enables the use of OLED displays
+ENCODER_ENABLE = yes        # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+LTO_ENABLE = yes            # Allow link time optimization
+TRACKBALL_ENABLE = yes      # Trackball
 POINTING_DEVICE_ENABLE = yes # Trackball
+RAW_ENABLE = yes            # Enable RAW HID communication to PC
+WPM_ENABLE = yes            # Bongocat!
+NO_USB_STARTUP_CHECK = yes  # Fix usb startup check for split keyboard
+
+CONSOLE_ENABLE = no         # Debug mode
+
+TAP_DANCE_ENABLE = no
+STENO_ENABLE = no
+BOOTMAGIC_ENABLE =no
+TERMINAL_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+KEY_LOCK_ENABLE = no
+AUDIO_ENABLE = no
+VELOCIKEY_ENABLE = no
